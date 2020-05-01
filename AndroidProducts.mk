0a6012f8109a67a79b7c36b9e361da84cb5a2fc7
ender1324 <ender1324@abv.bg>
2020-05-01 06:08:27 +0000

    a6010: Titanium bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6010.mk \
+    $(LOCAL_DIR)/titanium_a6010.mk \
--- titanium_a6010.mk
+++ titanium_a6010.mk
@@ -16,7 +16,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lenovo/a6010/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/titanium/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -26,7 +26,7 @@ TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm89
 
 TARGET_VENDOR := Lenovo
 PRODUCT_DEVICE := a6010
-PRODUCT_NAME := lineage_a6010
+PRODUCT_NAME := titanium_a6010
 BOARD_VENDOR := Lenovo
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := A6010
